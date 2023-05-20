83b271a943bfa3262142811856849dffce0532d9
yaredow <yareyilma11@gmail.com>
2023-05-20 14:16:19 +0300

    dom traversing moving up and down the dom

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 'use strict';
 
-///////////////////////////////////////
-// Modal window
-
+const btnScrollTo = document.querySelector('.btn--scroll-to');
+const section1 = document.querySelector('#section--1');
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+
+///////////////////////////////////////
+// Modal window
 
 const openModal = function () {
   modal.classList.remove('hidden');
@@ -30,26 +32,31 @@ document.addEventListener('keydown', function (e) {
   }
 });
 
-// ////////////////////////////////////////////////////////
-const header = document.querySelector('.header');
-console.log(header);
+// Button scrolling
+btnScrollTo.addEventListener('click', function (e) {
+  const sec1Cords = section1.getBoundingClientRect();
+  console.log();
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
 
-const message = document.createElement('div');
-message.classList.add('cookie-message');
-message.innerHTML =
-  "we use cookies for improved functionality and analytics. <button class='btn btn--close--cookie'>Got it</button>";
-header.append(message);
-// header.append(message);
-document
-  .querySelector('.btn--close--cookie')
-  .addEventListener('click', function () {
-    message.remove();
-  });
 
-message.style.backgroundColor = '#3333';
-message.style.width = '120%';
+// Navigation links scrolling
 
-document.documentElement.style.setProperty('--color-primary', 'orangered');
+document.querySelectorAll('.nav__links').forEach(function(el) {
+  el.addEventListener('click', function(e) {
+    e.preventDefault()
 
-const logo = document.querySelector('.nav__logo');
-console.log(logo.alt);
+    if(e.target.classList.contains('nav__link')) {
+      const id = e.target.getAttribute('href');  
+      document.querySelector(id).scrollIntoView({behavior: 'smooth'})
+
+    }
+  })
+})
+
+// ////////////////////////////////////////////////////////////////
+// 
+const h1 = document.querySelector('h1')
+console.log(h1.querySelectorAll('.highlight'));  
+console.log(h1.childNodes);
+h1.firstElementChild.style.color = 'white'
