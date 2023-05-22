37dd243f139839a5c91278c0d8d692829ddab0c2
yaredow <yareyilma11@gmail.com>
2023-05-22 14:19:49 +0300

    finish adding section revel while scrolling effect

--- script.js
+++ script.js
@@ -114,3 +114,22 @@ const headerObserver = new IntersectionObserver(stickyNav, {
   rootMargin: `-${navHeight}px`,
 });
 headerObserver.observe(header);
+
+// Section reveal while scrolling
+const sections = document.querySelectorAll('.section');
+const revealSection = function (entries) {
+  const [entry] = entries;
+  console.log(entry);
+  if (!entry.isIntersecting) return;
+  else entry.target.classList.remove('section--hidden');
+  observe.unobserve(entry.target);
+};
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+sections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
