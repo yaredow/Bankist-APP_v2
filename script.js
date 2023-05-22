0583f9a16a8d6ef59733d317d5d23551dfdd0be4
yaredow <yareyilma11@gmail.com>
2023-05-22 12:06:52 +0300

    add sticy nav using Intersection observer API

--- script.js
+++ script.js
@@ -98,9 +98,19 @@ nav.addEventListener('mouseout', function (e) {
   handleHover(e, 1);
 });
 
-// Sticky navigation
-const initalCoords = section1.getBoundingClientRect();
-window.addEventListener('scroll', function () {
-  if (window.scrollY > initalCoords.top) nav.classList.add('sticky');
+// Sticky navigation using the Intersection Observer API
+const header = document.querySelector('.header');
+const navHeight = nav.getBoundingClientRect().height;
+
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  if (!entry.isIntersecting) nav.classList.add('sticky');
   else nav.classList.remove('sticky');
+};
+
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+  rootMargin: `-${navHeight}px`,
 });
+headerObserver.observe(header);
